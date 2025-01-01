b2a9fd07e4263937b9bef6db3132e2137e12354f
Michael Brandon <mikebrandon91@gmail.com>
2025-01-01 17:24:23 -0600

    another one

--- backend/db/migrations/20250101221907-create-booking.js
+++ backend/db/migrations/20250101221907-create-booking.js
@@ -43,7 +43,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Bookings');
